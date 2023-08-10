24a058a8a6c81a25440d63d4c56aec2aed8ccde9
dolokelen <dolokelen1@gmail.com>
2023-08-11 03:36:00 +0800

    Configure mysql db

--- myschool/settings.py
+++ myschool/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'myschool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myschool',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Mosh@2022'
     }
 }
 
