d4d9180cf90d77ccf2673afe88b755ea39b9b180
dolokelen <dolokelen1@gmail.com>
2023-08-11 05:53:23 +0800

    Set verbose_name_plural on category and updated occupation choices

--- poll/models.py
+++ poll/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=200, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'categories'
     def __str__(self) -> str:
         return self.title
 
