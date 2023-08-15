882b756549e63b7fd344879242609f2480310fac
dolokelen <dolokelen1@gmail.com>
2023-08-16 05:08:27 +0800

    Override the QuestionSerializer create method to save question and choices

--- poll/serializers.py
+++ poll/serializers.py
@@ -14,7 +14,18 @@ class ChoiceSerializer(serializers.ModelSerializer):
 
 
 class QuestionSerializer(serializers.ModelSerializer):
-    choices = ChoiceSerializer(many=True, read_only=True)
+    choices = ChoiceSerializer(many=True)
     class Meta:
         model = models.Question
         fields = ['id', 'category', 'author', 'text', 'choices', 'comment', 'status']
+
+    def create(self, validated_data):
+        choice_data = validated_data.pop('choices')
+        question = models.Question.objects.create(**validated_data)
+        
+        for choice in choice_data:
+            models.Choice.objects.create(question=question, **choice)
+
+        return question
+
+  
